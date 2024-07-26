08ea72836af982b3b08737433944a3d8c9daa06c
bowloffruits <wen.ebc20@gmail.com>
2024-07-27 01:12:59 +0800

    Finalized timeseries.py

--- backend/timeseries.py
+++ backend/timeseries.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 
 aapl = yf.Ticker("AAPL")
 hist = aapl.history(period="5y")["Close"].reset_index()
-hist["Date"] = hist["Date"].apply(lambda x: x.strftime("%Y-%m-%d"))
-hist["Date"] = pd.to_datetime(hist["Date"], yearfirst=True)
-hist.set_index("Date", inplace=True)
 
-arima = ARIMA(hist, order=(1, 2, 1), seasonal_order=(1, 1, 1, 12)).fit()
+arima = ARIMA(hist["Close"], order=(1, 2, 1), seasonal_order=(1, 2, 1, 12)).fit()
 arima_forecast = arima.forecast(steps=30)
 
-history =
-#plt.plot(hist, color="blue", label="hist")
+print(arima_forecast)
+plt.plot(hist["Close"], color="blue", label="hist")
 plt.plot(arima_forecast, color="red", label="prediction")
+plt.xlim(1000)
 plt.show()
