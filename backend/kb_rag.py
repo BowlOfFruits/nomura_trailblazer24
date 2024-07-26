61e19a7b7730390d0c4a426a1631cb363f3efb25
bowloffruits <wen.ebc20@gmail.com>
2024-07-27 00:46:52 +0800

    Refactored kb_rag into a function for frontend api call

--- backend/kb_rag.py
+++ backend/kb_rag.py
@@ -33,47 +33,54 @@ bedrock_agent_runtime = boto3.client(
 )
 bedrock_runtime = boto3.client('bedrock-runtime', region_name=REGION)
 
-docs_only_response = bedrock_agent_runtime.retrieve(
-    knowledgeBaseId=KB_ID,
-    retrievalQuery={"text": QUERY},
-    retrievalConfiguration={
-        "vectorSearchConfiguration": {"numberOfResults": NUM_RESULTS,
+def get_docs(QUERY):
+    docs_only_response = bedrock_agent_runtime.retrieve(
+        knowledgeBaseId=KB_ID,
+        retrievalQuery={"text": QUERY},
+        retrievalConfiguration={
+            "vectorSearchConfiguration": {"numberOfResults": NUM_RESULTS,
                                       "overrideSearchType": "HYBRID"}
-    },
-)
+        },
+    )
 
-contexts = []
-for retrievedResult in docs_only_response["retrievalResults"]:
-    contexts.append(retrievedResult['content']['text'])
+    return docs_only_response
 
-#print(contexts)
+def get_llm_response(QUERY):
+    docs_only_response = get_docs(QUERY)
 
-prompt = f"""
-Human: You are a financial advisor, and provides answers to questions by using fact based and statistical information when possible. 
-Use the following pieces of information to provide an answer to the question enclosed in <question> tags. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    contexts = []
+    for retrievedResult in docs_only_response["retrievalResults"]:
+        contexts.append(retrievedResult['content']['text'])
 
-<context>
-{contexts}
-</context>
 
-<question>
-{QUERY}
-</question>
+    prompt = f"""
+    Human: You are a financial advisor, and provides answers to questions by using fact based and statistical information when possible. 
+    Use the following pieces of information to provide an answer to the question enclosed in <question> tags. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    
+    <context>
+    {contexts}
+    </context>
+    
+    <question>
+    {QUERY}
+    </question>
+    
+    The response should be specific.
+    
+    Assistant:"""
 
-The response should be specific.
+    # model paramters
+    llama2_payload = json.dumps({
+        "prompt": prompt,
+        "temperature": 0.5,
+        "top_p": 0.5
+    })
 
-Assistant:"""
+    modelId = 'meta.llama3-70b-instruct-v1:0' # change this to use a different version from the model provider
+    response = bedrock_runtime.invoke_model(body=llama2_payload, modelId=modelId)
+    response_body = json.loads(response.get('body').read())
+    
+    return response_body['generation']
 
-# payload with model paramters
-messages = [{"role": 'user', "content": [{'type': 'text', 'text': prompt.format(contexts, QUERY)}]}]
-llama2_payload = json.dumps({
-    "prompt": prompt,
-    "temperature": 0.5,
-    "top_p": 0.5
-})
-
-modelId = 'meta.llama3-70b-instruct-v1:0' # change this to use a different version from the model provider
-response = bedrock_runtime.invoke_model(body=llama2_payload, modelId=modelId)
-response_body = json.loads(response.get('body').read())
-print(response_body['generation'])
+get_llm_response(QUERY)
