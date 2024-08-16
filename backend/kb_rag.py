a118796423ded402fcd63cf397a385bfa66bd619
BowlOfFruits <wen.ebc20@gmail.com>
2024-08-16 14:46:42 +0800

    Edited kb_rag.py to use langchain instead of aws

--- backend/kb_rag.py
+++ backend/kb_rag.py
@@ -1,10 +1,34 @@
-import boto3
-import json
+import os
+from dotenv import load_dotenv
+from tqdm import tqdm
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
 
-KB_ID = "6R8TDE7AER"
-REGION = "us-east-1"
-MODEL = "anthropic.claude-3-haiku-20240307-v1:0"
+load_dotenv(dotenv_path="../api_keys.env")
 
+# Initialise the embedding model and vector db
+openai_embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+vector_store = Chroma(
+    collection_name="nomura_trailblazer24",
+    embedding_function=openai_embeddings,
+    persist_directory="./chroma_langchain_db"
+)
+
+# Load and chunk pdf documents
+for file in tqdm(os.listdir("pdf_outputs"), "Processing pdfs"):
+    loader = PyPDFLoader(f"pdf_outputs/{file}") # Load
+    pages = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50) # Chunk
+    splits = text_splitter.split_documents(pages)
+
+    vector_store.add_documents(documents=splits, embeddings=openai_embeddings) # Add into vector store
+
+
+
+'''
 # From the frontend client profile settings
 client_profile = {
     "risk_tolerance": "low",
@@ -19,81 +43,39 @@ Based on their news and my profile, recommend if I should buy, sell or hold each
 Also recommend other stocks that can help to diversify my portfolio according to my profile and summarise their news.
 """
 
-NUM_RESULTS = 20
 
-# Setup bedrock
-bedrock_agent_runtime = boto3.client(
-    service_name="bedrock-agent-runtime",
-    region_name=REGION,
-)
+text = f"""
+I have these following stocks: {', '.join(client_profile['current_portfolio'])}. 
 
-bedrock_runtime = boto3.client(
-    "bedrock-runtime",
-    region_name=REGION,
-)
-
-
-def get_docs(QUERY, client_profile):
-    text = f"""
-    I have these following stocks: {', '.join(client_profile['current_portfolio'])}. 
-
-    My profile is:
-    Risk Tolerance: {client_profile['risk_tolerance']}
-    Preferred Sectors: {', '.join(client_profile['preferred_sectors'])}
-    Investment Horizon: {client_profile['investment_horizon']}
+My profile is:
+Risk Tolerance: {client_profile['risk_tolerance']}
+Preferred Sectors: {', '.join(client_profile['preferred_sectors'])}
+Investment Horizon: {client_profile['investment_horizon']}
     
-    {QUERY}
-    """
-    docs_only_response = bedrock_agent_runtime.retrieve(
-        knowledgeBaseId=KB_ID,
-        retrievalQuery={"text": text},
-        retrievalConfiguration={
-            "vectorSearchConfiguration": {
-                "numberOfResults": NUM_RESULTS,
-                "overrideSearchType": "HYBRID",
-            }
-        },
-    )
+{QUERY}
+"""
 
-    return docs_only_response
+prompt = f"""
+Human: You are a financial advisor, and provides answers to questions by using fact based and statistical information when possible. 
+Use the following pieces of information to provide an answer to the question enclosed in <question> tags. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
+<context>
 
-def get_llm_response(QUERY, client_profile):
-    docs_only_response = get_docs(QUERY, client_profile)
+</context>
 
-    contexts = []
-    for retrievedResult in docs_only_response["retrievalResults"]:
-        contexts.append(retrievedResult["content"]["text"])
+<question>
+I have these following stocks: {', '.join(client_profile['current_portfolio'])}. 
 
-    prompt = f"""
-    Human: You are a financial advisor, and provides answers to questions by using fact based and statistical information when possible. 
-    Use the following pieces of information to provide an answer to the question enclosed in <question> tags. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    
-    <context>
-    {contexts}
-    </context>
-    
-    <question>
-    I have these following stocks: {', '.join(client_profile['current_portfolio'])}. 
+My profile is:
+Risk Tolerance: {client_profile['risk_tolerance']}
+Preferred Sectors: {', '.join(client_profile['preferred_sectors'])}
+Investment Horizon: {client_profile['investment_horizon']}
 
-    My profile is:
-    Risk Tolerance: {client_profile['risk_tolerance']}
-    Preferred Sectors: {', '.join(client_profile['preferred_sectors'])}
-    Investment Horizon: {client_profile['investment_horizon']}
-    
-    {QUERY}
-    </question>
-    
-    The response should be specific.
-    
-    Assistant:"""
+{QUERY}
+</question>
 
-    # model paramters
-    llama2_payload = json.dumps({"prompt": prompt, "temperature": 0.5, "top_p": 0.5})
+The response should be specific.
 
-    modelId = "meta.llama3-70b-instruct-v1:0"  # change this to use a different version from the model provider
-    response = bedrock_runtime.invoke_model(body=llama2_payload, modelId=modelId)
-    response_body = json.loads(response.get("body").read())
-
-    return response_body["generation"]
+Assistant:"""
+'''
